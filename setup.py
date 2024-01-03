6ad9bde56e5ae3ba6a9e2c721d0c9cba2b8da01e
Henrique Schmitz <schhenrique@gmail.com>
2024-01-03 20:02:14 -0300

    chore: add modules to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='VideoBackgroundExtractor',
     version='1.0.0',
     author="Henrique Schmitz",
-    packages=find_packages()
+    packages=find_packages(),
+    py_modules=['VideoBackgroundExtractor']
 )
