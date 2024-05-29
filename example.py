fb2b4f271c58ee07f7ad20fc5b04081490c1df64
Henrique Schmitz <schhenrique@gmail.com>
2024-05-28 23:20:24 -0300

    docs: fix example

--- example.py
+++ example.py
@@ -1,27 +1,27 @@
 import cv2
-import subprocess
-subprocess.run(["git", "clone", "https://www.github.com/HenriqueSchmitz/VideoBackgroundExtractor.git"])
-from VideoBackgroundExtractor import VideoBackgroundExtractor
+from video_background_extractor import VideoBackgroundExtractor
 
-videoPath = "VideoBackgroundExtractor/example.mp4"
+
+videoPath = "example.mp4"
 video = cv2.VideoCapture(videoPath)
 extractor = VideoBackgroundExtractor()
 
 exampleFrameNumber = 3000
 video.set(cv2.CAP_PROP_POS_FRAMES, exampleFrameNumber)
 ret, exampleFrame = video.read()
-cv2.imshow(exampleFrame)
+cv2.imshow("Example Frame", exampleFrame)
 
 extractor.loadVideo(video)
 
 background = extractor.getBackgroundFrame()
-cv2.imshow(background)
+cv2.imshow("Background", background)
 
 frameDifference = extractor.getDifferenceToBackground(exampleFrame)
-cv2.imshow(frameDifference)
+cv2.imshow("Example Difference to Background", frameDifference)
 
 differenceIndex = extractor.getDifferenceIndex(exampleFrame)
 print(differenceIndex)
 
 noBackgroundFrame = extractor.removeBackground(exampleFrame)
-cv2.imshow(noBackgroundFrame)
+cv2.imshow("Example Frame with Removed Background", noBackgroundFrame)
+cv2.waitKey(0)
